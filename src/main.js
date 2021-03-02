a4124c96bfb907622894f0ea2e2e88a648981e30
jianghjs <jianghjs@foxmail.com>
2021-03-02 14:38:53 +0800

    element

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import {Input, Select, Button} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Button)
 
 /* eslint-disable no-new */
 new Vue({
